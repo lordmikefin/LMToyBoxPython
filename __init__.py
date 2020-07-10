e271b5915fee4cddd846b309629a4693e25c34e6
Lord Mike <lordmike@iki.fi>
2020-07-10 12:41:55 +0300

    Added function indent(...)

--- __init__.py
+++ __init__.py
@@ -85,3 +85,25 @@ def write_lines_to_file(file: str, lines: list, mode='w'):
     """
     with open(file, mode) as f:
         f.writelines(lines)
+
+
+def indent(elem, level=0):
+    ''' Indent the xml tree '''
+    # TODO: this should be part of 'xml.etree.ElementTree'
+    # TODO: Send request to ElementTree toolkit project. Python core project?
+
+    # NOTE: code copied from stackoverflow
+    # https://stackoverflow.com/questions/3095434/inserting-newlines-in-xml-file-generated-via-xml-etree-elementtree-in-python
+    i = "\n" + level*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i

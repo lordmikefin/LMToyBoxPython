3fe44ef956728a8edeaad171759a4e3b66926991
Lord Mike <lordmike@iki.fi>
2020-07-23 11:14:44 +0300

    created _hash_core() -function

--- LMhashlib.py
+++ LMhashlib.py
@@ -13,6 +13,7 @@ import hashlib
 from tqdm import tqdm
 from pathlib import Path
 from . import logger
+from builtins import Exception
 
 
 def print_progress(calculated, file_len):
@@ -24,55 +25,45 @@ def print_progress(calculated, file_len):
 
 def md5sum(src: str, length: int=io.DEFAULT_BUFFER_SIZE, callback=None,
            show_progress: bool=False) -> str:
+    ''' Calculate md5 checksum.
+      NOTE: Use SHA-2 or SHA-3 instead. For serucity.
     '''
-    Calculate md5 checksum.
-
-    https://www.geeksforgeeks.org/md5-hash-python/
-    https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
-      NOTE: "The underlying MD5 algorithm is no longer deemed secure"
-      TODO: Use SHA-2 or SHA-3 instead. For serucity.
-
-    https://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python/40961519#40961519
-    '''
-    file_len = Path(src).stat().st_size
-    pbar = None
-    if show_progress:
-        pbar = tqdm(total=file_len)
-    calculated = 0
-    md5 = hashlib.md5()
-    with io.open(src, mode="rb") as fd:
-        for chunk in iter(lambda: fd.read(length), b''):
-            md5.update(chunk)
-            if not callback is None:
-                calculated += len(chunk)
-                callback(calculated, file_len)
-            elif pbar:
-                pbar.update(len(chunk))
-
-    if pbar:
-        pbar.close()
-    # return md5
-    return md5.hexdigest()
+    logger.debug('Calculate md5 sum for file: ' + str(src))
+    return _hash_core(src, length, callback, show_progress, hashlib.md5())
 
 
 def sha256(src: str, length: int=io.DEFAULT_BUFFER_SIZE, callback=None,
            show_progress: bool=False) -> str:
-    '''
-    Calculate sha256
-
-    based on md5sum(...) -function
-    https://docs.python.org/2/library/hashlib.html
-    '''
+    ''' Calculate sha256 '''
     logger.debug('Calculate sha256 sum for file: ' + str(src))
+    return _hash_core(src, length, callback, show_progress, hashlib.sha256())
+
+
+def _hash_core(src: str, length: int=io.DEFAULT_BUFFER_SIZE, callback=None,
+           show_progress: bool=False, hasher=None) -> str:
+    '''
+    Core function to calculate the has.
+
+    https://docs.python.org/3/library/hashlib.html
+
+    https://www.geeksforgeeks.org/md5-hash-python/
+    https://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
+      NOTE: "The underlying MD5 algorithm is no longer deemed secure"
+
+    https://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python/40961519#40961519
+    '''
+    if hasher is None:
+        raise Exception('"hasher" parameter must be defined')
+
     file_len = Path(src).stat().st_size
     pbar = None
     if show_progress:
         pbar = tqdm(total=file_len)
     calculated = 0
-    md5 = hashlib.sha256()
     with io.open(src, mode="rb") as fd:
+        # TODO: optimize - remove 'if-else' from the loop 
         for chunk in iter(lambda: fd.read(length), b''):
-            md5.update(chunk)
+            hasher.update(chunk)
             if not callback is None:
                 calculated += len(chunk)
                 callback(calculated, file_len)
@@ -82,4 +73,4 @@ def sha256(src: str, length: int=io.DEFAULT_BUFFER_SIZE, callback=None,
     if pbar:
         pbar.close()
 
-    return md5.hexdigest()
+    return hasher.hexdigest()

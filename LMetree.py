d8ad0d554b24e67a55177c205a4056d5f6c9b6df
Lord Mike <lordmike@iki.fi>
2020-07-29 12:51:09 +0300

    Type hinting with Sphinx docstrings/comments

--- LMetree.py
+++ LMetree.py
@@ -14,7 +14,7 @@ from xml.etree.ElementTree import Element, SubElement
 def create_subelem(parent: Element, tag_name: str=None, text: str=None):
     if not text:
         return  # do not create empty element
-    elem = SubElement(parent, tag_name)
+    elem = SubElement(parent, tag_name) #: :type elem: Element
     elem.text = text
 
 
